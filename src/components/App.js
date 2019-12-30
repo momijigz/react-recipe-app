c7acbc4ff324b59ff7c7daa3820269464160870b
Moazzam Khan <moazzam.khan.wa@gmail.com>
2019-12-30 23:43:30 +0500

    Added edamam

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import React, { useEffect, useState } from 'react';
 
 const App = () => {
 
-  const APP_ID = '343de82d';
-  const APP_KEY = '796cc92800c1a1281a1b047885a39830';
+  const APP_ID = '';
+  const APP_KEY = '';
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
   const [counter, setCounter] = useState(0);
@@ -16,9 +16,9 @@ const App = () => {
   }, []);
 
   const getRecipes = async () => {
-    // const response = await fetch(exampleReq);
-    // const data = await response.json();
-    // console.log(data)
+    const response = await fetch(exampleReq);
+    const data = await response.json();
+    console.log(data)
     setRecipes(data.hits)
   }
   return (
